b94b724715ea9c4cde9946753cee98ddc8c6e70a
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-02-06 17:51:53 -0300

    feat: working on File processing

--- src/app/core/services/currciulo.service.ts
+++ src/app/core/services/currciulo.service.ts
@@ -4,6 +4,7 @@ import { ContentI } from '../../shared/models/ContentI';
 import { BehaviorSubject, Observable, ReplaySubject, map, of, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { FormsTrabalheConoscoI } from 'src/app/shared/models/FormsTrabalheConoscoI';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -11,11 +12,22 @@ import { FormsTrabalheConoscoI } from 'src/app/shared/models/FormsTrabalheConosc
 })
 export class CurriculoService {
   private readonly API_URL = 'http://localhost:8081/v1/prospects';
+
+  localStorage = window.localStorage;
   
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private cookie:CookieService) { }
   
   send(forms:FormsTrabalheConoscoI){
+    this.saveCurririulo(forms.curriculoFile as File);
     return this.http.post(`${this.API_URL}/curriculo`,forms);
+  }
+
+  saveCurririulo(file:File){
+    this.cookie.set
+    const formData = new FormData();
+    formData.append('file',file);
+    this.cookie.set('curriculo',JSON.stringify(formData));
+    return formData;
   }
 
 
