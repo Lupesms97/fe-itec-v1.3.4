674f375a3983ccbaf3aa2a3657bf6604c979d18e
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-03-13 12:47:30 -0300

    update varaibles of environmets

--- src/app/environments/variables.environments.ts
+++ src/app/environments/variables.environments.ts
@@ -3,8 +3,8 @@ export const environment = {
     ROLES_KEY :'_rly-usr',
     USER_NAME :'name',
     api_url_auth: 'https://authenticationmodule-core-production.up.railway.app/v2/auth',
-    api_url_ombudsman: 'https://be-itec-v141-production.up.railway.app//v1/opinions',
-    api_url_curriculo: 'https://be-itec-v141-production.up.railway.app//v1/employees',
-    api_url_forms: 'https://be-itec-v141-production.up.railway.app//v1/prospects',
+    api_url_ombudsman: 'https://be-itec-v141-production.up.railway.app/v1/opinions',
+    api_url_curriculo: 'https://be-itec-v141-production.up.railway.app/v1/employees',
+    api_url_forms: 'https://be-itec-v141-production.up.railway.app/v1/prospects',
     
 }
